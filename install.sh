dddf9e6733bb2e049655d6c51b615883edf1f7ea
sam <samosborn88@gmail.com>
2020-02-13 20:04:12 -0800

    starts fleshing out aliases and other pieces

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 echo test
 
-wget some short url or list of urls
-move downloaded file to .bash_custom_profile
+echo wget some short url or list of urls
+echo move downloaded file to .bash_custom_profile
 echo if statement that will source custom profile if it exists, into .bashrc
+echo search the bashrc for sourcing bash_aliases, if it exists, just move aliases file, otherwise append sourcing aliases file 
 
+echo grep -E bash_custom_profile ~/.bashrc  => if doesnt exist, append to file
+printf "source \"\$HOME/.bash_custom_profile\"\n" 
+source "$HOME/.bashrc" # source the file just in case
