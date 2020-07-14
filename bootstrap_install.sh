9e8a0396bb76dcd650b6b48a8bc61d2951f90f12
Sam Osborn <samosborn88@gmail.com>
2020-07-13 22:06:02 -0500

    updates bootstrap to make git prereq

--- bootstrap_install.sh
+++ bootstrap_install.sh
@@ -3,20 +3,17 @@
 TARGET_SETUP_DIR="$HOME/temporary_env_setup_files"
 printf "target setup dir to bootstrap files into: %s\n" $TARGET_SETUP_DIR
 
-if [[ -d $TARGET_SETUP_DIR ]]; then
-    printf "target setup directory already exists\n"
-    exit 1;
+if ! git --version 2>&1 >/dev/null; then 
+  printf "Git not installed, exiting...\n"
+  exit 1;  
 fi
 
-if git --version 2>&1 >/dev/null; then 
-    git clone https://github.com/capttrousers/dotneedful.git $TARGET_SETUP_DIR
+if [[ -d $TARGET_SETUP_DIR ]]; then
+    printf "target setup directory already exists\n"
+    printf "get the latest from git repo, then rerun install.sh\n"
+    exit 1;
 else 
-    wget -q https://github.com/capttrousers/dotneedful/archive/master.zip
-    unzip -q master.zip -d $TARGET_SETUP_DIR
-fi 
-
-# echo for now, bootstrapping will just be copying the files from local repo
-# mkdir -p $TARGET_SETUP_DIR
-# cp -a $HOME/code/dotneedful/* $TARGET_SETUP_DIR
+    git clone https://github.com/capttrousers/dotneedful.git $TARGET_SETUP_DIR
+fi
 
 printf "\n  Then run %s/install.sh\n" $TARGET_SETUP_DIR
